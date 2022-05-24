f4c8e107dba0007bcee1b9726a2d6a6c89035cb6
ALEKSANDR GOLIKOV <10749+goalrazor@users.noreply.github.yandex-team.ru>
2022-05-24 18:46:26 +0300

    delete react premade add project initial structure

---
+++ src/components/app-header/AppHeader.js
@@ -0,0 +1,11 @@
+import {Logo} from '@ya.praktikum/react-developer-burger-ui-components'
+
+const AppHeader = () => {
+    return (
+        <header>
+            <Logo/>
+        </header>
+    );
+}
+
+export default AppHeader;
