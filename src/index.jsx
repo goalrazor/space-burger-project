43a80783c0093f1b363bb0eb552b3173fd0952e1
ALEKSANDR GOLIKOV <10749+goalrazor@users.noreply.github.yandex-team.ru>
2022-08-11 17:55:52 +0300

    инициализация webSocket

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,22 @@ import {rootReducer} from './services/reducers';
 import thunk from 'redux-thunk';
 import App from "./components/app/App";
 import {BrowserRouter as Router} from "react-router-dom";
+import {socketMiddleware} from './services/middleware/socketMiddleware'
 
 const composeEnhancers =
     typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
         ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
         : compose;
 
-const enhancer = composeEnhancers(applyMiddleware(thunk));
+const ordersWsAll = "wss://norma.nomoreparties.space/orders/all"
+const ordersWsUser = "wss://norma.nomoreparties.space/orders"
+
+const enhancer = composeEnhancers(applyMiddleware(
+        thunk,
+        socketMiddleware(ordersWsAll),
+        socketMiddleware(ordersWsUser),
+    )
+);
 
 const store = createStore(rootReducer, enhancer);
 
--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import {orderDetailsReducer} from "./burger-constructor-ingredients"
 import {ingredientReducer} from "./burger-ingredients"
 import {authReducer} from "./auth";
+import {wsReducer} from "./webSocket";
 
 export const rootReducer = combineReducers({
     ingredientReducer,
     orderDetailsReducer,
-    authReducer
+    authReducer,
+    wsReducer
 })
