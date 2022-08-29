2c807bde42629ea8aa54d12fdd2d3d17858fc4ff
ALEKSANDR GOLIKOV <10749+goalrazor@users.noreply.github.yandex-team.ru>
2022-08-29 14:23:30 +0300

    типизирован BurgerConstructorActions

--- src/services/types/redux.ts
+++ src/services/types/redux.ts
@@ -2,10 +2,11 @@ import {store} from "../../index";
 import {TAuthActions} from "../actions/auth/authActions";
 import {Action, ActionCreator} from "redux";
 import {ThunkAction} from "redux-thunk";
+import {TBurgerConstructorIngredientsActions} from "../actions/burger-constructor-ingredients";
 
 export type RootState = ReturnType<typeof store.getState>;
-type TApplicationActions = TAuthActions;
+type TApplicationActions = TAuthActions | TBurgerConstructorIngredientsActions;
 
-export type AppThunk<TReturn = Promise<any>> = ActionCreator<ThunkAction<TReturn, Action, RootState, TApplicationActions>>;
+export type AppThunk<TReturn = Promise<any> | void> = ActionCreator<ThunkAction<TReturn, Action, RootState, TApplicationActions>>;
 
 export type AppDispatch = typeof store.dispatch;
